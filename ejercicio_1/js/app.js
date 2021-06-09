164b152dfc7d725037596790eca91d5f85238a54
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2021-06-09 15:20:56 -0500

    ejercicio_tabla

---
+++ ejercicio_1/js/app.js
@@ -0,0 +1,82 @@
+const users = [
+    {
+        name: 'Erik',
+        age: 29,
+        email: 'erik@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/erik' },
+            { name: 'twitter', url: 'twitter/erik' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Georg',
+        age: 33,
+        email: 'georg@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/georg' },
+            { name: 'twitter', url: 'twitter/georg' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Andrea',
+        age: 42,
+        email: 'andrea@hotmail.com',
+        social: [
+            { name: 'facebook', url: 'facebook/andrea' },
+            { name: 'twitter', url: 'twitter/andrea' }
+        ],
+        gender: 'Female'
+    },
+    {
+        name: 'Oscar',
+        age: 31,
+        email: 'oscar@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/oscar' },
+            { name: 'twitter', url: 'twiter/oscar' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Daniela',
+        age: 22,
+        email: 'andrea@uaq.mx',
+        social: [
+            { name: 'facebook', url: 'facebook/andrea' },
+            { name: 'twitter', url: 'twitter/andrea' }
+        ],
+        gender: 'Female'
+    },
+]
+
+
+/*obtener todos los usuarios con el correo academlo*/
+const axu = users.filter((usersFilterByEmail) => usersFilterByEmail.email.endsWith('@academlo.com'))
+
+/*la creacion del elemento tbody en la table*/
+function createElemenTbodyInTable(){
+    const elementTable = document.getElementById('dataUser')
+    const elementTbody = document.createElement('tbody')
+    elementTable.appendChild(elementTbody)
+    return elementTbody
+}
+
+/*Pintar datos del user en el DOM*/
+function pinterData(element,dataUser){
+    tbody = element()
+    dataUser.forEach(data => {
+        tbody.innerHTML += `
+        <tr>
+        <td>${data.name}</td>
+        <td>${data.age}</td>
+        <td>${data.email}</td>
+        <th>${data.social[0].url}</td>
+        <th>${data.social[1].url}</td>
+        </tr>
+        `
+    });
+}
+
+pinterData(createElemenTbodyInTable, users.filter((usersFilterByEmail) => usersFilterByEmail.email.endsWith('@academlo.com')))
