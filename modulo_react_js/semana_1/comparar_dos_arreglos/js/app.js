6180c173eb493ca3a4c1881123ab15187020a0e1
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2021-06-23 16:33:13 -0500

    cajitas

---
+++ modulo_react_js/semana_1/comparar_dos_arreglos/js/app.js
@@ -0,0 +1,13 @@
+const array1 = [1,3,4,5,2,11]
+const array2 = [3,10,1,2,4,5]
+const newArray = []
+
+const comparatorArray = () =>{
+   
+    if(array1.length > array2.length){
+        
+    }
+
+    return newArray
+}
+console.log(comparatorArray())
